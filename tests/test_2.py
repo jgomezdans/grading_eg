4e3405f82d00c8b8b5ae0d6d22068ef8996639c7
Jose Gomez-Dans <jgomezdans@gmail.com>
2020-07-13 17:04:17 +0100

    Sensible test...

--- tests/test_2.py
+++ tests/test_2.py
@@ -10,4 +10,4 @@ from assignment2 import assignment_two
 def test_two():
     sample_string = "A boring tedious string"
     sol = sample_string.replace("ri", "#")
-    assert sol == assignment_two(sample_string)
+    assert sol == assignment_two(sample_string, "ri")
